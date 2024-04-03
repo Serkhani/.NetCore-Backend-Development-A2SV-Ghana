bd6a52ec19271c8ab2c21996aacbd9a25c45f2fa
Serkhani <dak.boniface@gmail.com>
2024-04-03 17:39:48 +0000

    added task2 -Day2

---
+++ Day2/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Dictionary<string, int> WordFreqCount(string words)
+{
+    Dictionary<string, int> wordFreq = new Dictionary<string, int>();
+    words.Split(' ').ToList().ForEach(word =>
+    {
+        if (wordFreq.ContainsKey(word.ToLower()))
+            wordFreq[word.ToLower()]++;
+        else
+            wordFreq[word.ToLower()] = 1;
+    });
+    return wordFreq;
+}
+Dictionary<string, int> wordFreqCount = WordFreqCount("I am a boy");
+IEnumerable<string> wordFreqPairs =
+from wordFreq in wordFreqCount
+select $"{wordFreq.Key}: {wordFreq.Value}";
+
+foreach (var item in wordFreqPairs)
+{
+    Console.WriteLine(item);
+}
