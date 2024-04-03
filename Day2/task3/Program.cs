df51c7862648625bab166bcddf8a3fb950c34e20
Serkhani <dak.boniface@gmail.com>
2024-04-03 21:19:19 +0000

    added task3 -Day2

---
+++ Day2/task3/Program.cs
@@ -0,0 +1,33 @@
+﻿    bool PalindromeChecker(string text)
+    {
+        string retStr = "";
+        foreach (char ch in text)
+        {
+            int ascii_val = (int)ch;
+            if ((97 <= ascii_val && ascii_val <= 122) || (48 <= ascii_val && ascii_val <= 57))
+            {
+                retStr += ch;
+            }
+            else if (65 <= ascii_val && ascii_val <= 90)
+            {
+                retStr += (char)(ascii_val + 32);
+            }
+        }
+        retStr = retStr.ToLower();
+        for (int l = 0; l < retStr.Length / 2; l++)
+        {
+            if (retStr[l] != retStr[retStr.Length - l - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Psuedo-Testing
+    Console.WriteLine(PalindromeChecker("A man, a plan, a canal: Panama")==true)
+    Console.WriteLine(PalindromeChecker("race a car")==false)
+    Console.WriteLine(PalindromeChecker("")==true)
+    Console.WriteLine(PalindromeChecker(" ")==true)
+    Console.WriteLine(PalindromeChecker("12321")==true)
+    Console.WriteLine(PalindromeChecker("not a palindrome")==false)
